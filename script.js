b7f002185dcb9592071ae151cd353614f00b71db
Alekh Kumar <alekhkumar777@gmail.com>
2021-11-19 21:07:21 +0530

    Add boundary for players

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let P1_UP = 87;
-let P1_DOWN = 83;
-let P1_CTRL = 17;
-let P2_UP = 38;
-let P2_DOWN = 40;
-let P2_CTRL = 191;
+let P1_UP = 87; // 'W'
+let P1_DOWN = 83; // 'S'
+let P1_CTRL = 86; // 'V'
+let P2_UP = 38; // 'UP_ARROW'
+let P2_DOWN = 40; // 'DOWN_ARROW'
+let P2_CTRL = 191; // '/'
 let PLAYER_MOVEMENT = 4;
 
 let player1 = document.getElementsByClassName("player1")[0];
 let player2 = document.getElementsByClassName("player2")[0];
+let laser = document.getElementsByClassName("laser")[0];
 
 let p1y = window.innerHeight / 2 - 35;
 let p2y = window.innerHeight / 2 - 35;
@@ -26,9 +27,6 @@ function toggleKey(keyCode, isPressed) {
     if (keyCode == P2_DOWN) {
         controller.p2Down = isPressed;
     }
-    // if (keyCode == SPACE_KEY) {
-    //     controller.space = isPressed;
-    // }
 }
 
 let controller = new Object();
@@ -46,6 +44,8 @@ function handleControls() {
     if (controller.p2Down) {
         p2y += PLAYER_MOVEMENT;
     }
+
+    setBoundary();
 }
 
 function setPosition() {
@@ -59,10 +59,16 @@ function loop() {
     if (new Date().getTime() - lastLoopRun > 15) {
         handleControls();
         setPosition();
-
         lastLoopRun = new Date().getTime();
     }
     setTimeout(loop, 2);
+}
+
+function setBoundary(){
+    if(p1y < 20) p1y = 20;
+    if(p1y > (window.innerHeight - 20 - 35 - 20 - 20)) p1y = window.innerHeight - 20 - 35 - 20 - 20;
+    if(p2y < 20) p2y = 20;
+    if(p2y > (window.innerHeight - 20 - 35 - 20 - 20)) p2y = window.innerHeight - 20 - 35 - 20 - 20;
 }
 
 loop();
